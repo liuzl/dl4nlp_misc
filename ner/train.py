23534555918fba9f83a3829fa9fdafcfeade8ce3
liuzhanliang <zhanliangliu@qq.com>
2019-08-04 08:44:23 +0800

    update jp

--- ner/train.py
+++ ner/train.py
@@ -1,10 +1,10 @@
 import nagisa
 
 # After finish training, save the three model files (*.vocabs, *.params, *.hp).
-nagisa.fit(train_file="sample.train", dev_file="sample.dev", test_file="sample.test", model_name="sample")
+nagisa.fit(train_file="jp/sample.train", dev_file="jp/sample.dev", test_file="jp/sample.test", model_name="jp/sample")
 
 # Build the tagger by loading the trained model files.
-sample_tagger = nagisa.Tagger(vocabs='sample.vocabs', params='sample.params', hp='sample.hp')
+sample_tagger = nagisa.Tagger(vocabs='jp/sample.vocabs', params='jp/sample.params', hp='jp/sample.hp')
 
 text = "福岡・博多の観光情報"
 words = sample_tagger.tagging(text)
