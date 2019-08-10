95f418af497cf49c1e2a84e48e6abdba7d240248
liuzl <zhanliangliu@qq.com>
2019-08-10 10:35:45 +0000

    update client

--- chitchat/static/js/client.js
+++ chitchat/static/js/client.js
@@ -1,63 +1,46 @@
 $(document).ready(function() {
-    var conn;
-    var msg = document.getElementById("msg");
-    var log = document.getElementById("log");
-
-    function Join() {
-        var uid = $("#uid").val().trim();
-        if (uid == "") {
-            alert("Your uid");
-            return;
-        }
-        if (window["WebSocket"]) {
-            conn = new WebSocket("ws://" + document.location.host + "/ws?uid="+encodeURIComponent(uid));
-            conn.onclose = function (evt) {
-                var item = document.createElement("div");
-                item.innerHTML = "<b>Connection closed.</b>";
-                appendLog(item);
-            };
-            conn.onmessage = function (evt) {
-                var messages = evt.data.split('\n');
-                for (var i = 0; i < messages.length; i++) {
-                    var item = document.createElement("div");
-                    item.innerText = messages[i];
-                    appendLog(item);
-                }
-            };
-        } else {
-            var item = document.createElement("div");
-            item.innerHTML = "<b>Your browser does not support WebSockets.</b>";
-            appendLog(item);
-        }
-    }
-
-    function appendLog(item) {
-        var doScroll = log.scrollTop > log.scrollHeight - log.clientHeight - 1;
-        log.appendChild(item);
-        if (doScroll) {
-            log.scrollTop = log.scrollHeight - log.clientHeight;
-        }
-    }
-
-    document.getElementById("form").onsubmit = function () {
-        if (!conn) {
-            alert("Join first");
-            return false;
-        }
-        if (!msg.value) {
-            return false;
-        }
-        conn.send(msg.value);
-        msg.value = "";
-        return false;
-    };
+    Join();
     $('#submit').click(function() {
-        Join();
+        var text = $('#text').val().trim();
+        if (text != "") {
+            Parse(text);
+        }
     });
-    $("#name").on('keypress', function(e) {
+    $("#text").on('keypress', function(e) {
         if (e.keyCode != 13) return;
-        Join();
+        var text = $('#text').val().trim();
+        if (text != "") {
+            Parse(text);
+        }
     });
 });
 
+function Parse(text) {
+    $.ajax({
+        url: "./api?text="+encodeURIComponent(text), cache: false,
+        success: function(result) {
+            var item = document.createElement("div");
+            item.innerHTML = result["message"];
+            appendLog(item);
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+            alert(XMLHttpRequest.responseText);
+        }
+    });
+}
+
+function appendLog(item) {
+    var log = document.getElementById("log");
+    var doScroll = log.scrollTop > log.scrollHeight - log.clientHeight - 1;
+    log.appendChild(item);
+    if (doScroll) {
+        log.scrollTop = log.scrollHeight - log.clientHeight;
+    }
+}
+
+function Join() {
+    var item = document.createElement("div");
+    item.innerHTML = "<b>你好，主人！</b>";
+    appendLog(item);
+}
 
